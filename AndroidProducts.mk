30ace648ca55f9c3f6b25d67b04c8723bb2a8117
明宇 <69169091+XMing673@users.noreply.github.com>
2025-11-25 17:17:50 +0800

    omni 2 twrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_rk3566_rgo.mk
+    $(LOCAL_DIR)/twrp_rk3566_rgo.mk
 
 COMMON_LUNCH_CHOICES := \
     twrp_rk3566_rgo-user \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_rk3566_rgo-user
-add_lunch_combo omni_rk3566_rgo-userdebug
-add_lunch_combo omni_rk3566_rgo-eng
+add_lunch_combo twrp_rk3566_rgo-user
+add_lunch_combo twrp_rk3566_rgo-userdebug
+add_lunch_combo twrp_rk3566_rgo-eng
